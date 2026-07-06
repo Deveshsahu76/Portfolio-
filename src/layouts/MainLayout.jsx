24001a6c30ba0354df11cccd43b4b731703b5ee3
Devesh sahu <deveshsahu567@gmail.com>
2026-07-06 12:58:10 +0530

    Fix layout outlet rendering

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,19 +1,26 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 import Navbar from '../components/Navbar'
-import SocialRail from '../components/SocialRail'
 import Footer from '../components/Footer'
+import LeftSidebar from '../components/LeftSidebar'
+import CustomCursor from '../components/CustomCursor'
 
-export default function MainLayout({ children }) {
+export default function MainLayout() {
   return (
-    <div className="relative min-h-screen overflow-hidden">
-      <div className="floating-orb left-8 top-28 h-5 w-5 bg-violet-500" />
-      <div className="floating-orb right-12 top-36 h-7 w-7 bg-cyan-400" />
-      <div className="floating-orb bottom-24 left-1/3 h-4 w-4 bg-blue-500" />
+    <div className="min-h-screen overflow-x-hidden bg-slate-50 text-slate-950 antialiased dark:bg-slate-950 dark:text-white">
+      <CustomCursor />
 
       <Navbar />
-      <SocialRail />
-      <main>{children}</main>
-      <Footer />
+
+      <LeftSidebar />
+
+      <div className="flex min-h-[calc(100vh-96px)] flex-col">
+        <div className="flex-1">
+          <Outlet />
+        </div>
+
+        <Footer />
+      </div>
     </div>
   )
 }
